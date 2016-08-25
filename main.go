556c1a3c929593eec9f9bb76264fe66e824babac
Armando Cerna <armandocerna@gmail.com>
2016-08-24 22:18:56 -0700

    use flags instead of args

--- main.go
+++ main.go
@@ -1,20 +1,24 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
-	"os"
 )
 
 func main() {
-	targetHost := fmt.Sprintf("%s:%s", os.Args[2], os.Args[3])
-	proxyPort := os.Args[1]
+	proxyPort := flag.String("proxy-port", "9090", "Port that capitanhook proxy binds to")
+	targetURL := flag.String("target-host", "google.com", "Target URL to proxy to")
+	targetPort := flag.String("target-port", "80", "Target port to proxy to")
+	flag.Parse()
+	targetHost := fmt.Sprintf("%s:%s", targetURL, targetPort)
+	listenString := fmt.Sprintf(":%f", proxyPort)
 	proxy := httputil.NewSingleHostReverseProxy(&url.URL{
 		Scheme: "http",
 		Host:   targetHost,
 	})
-	fmt.Printf("The capitan is listening")
-	http.ListenAndServe(":"+proxyPort, proxy)
+	fmt.Printf("The capitan is listening %v", listenString)
+	http.ListenAndServe(listenString, proxy)
 }
