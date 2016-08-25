b55ad1ac0662fac73dff8bb26976c6d669fa6445
Armando Cerna <armandocerna@gmail.com>
2016-08-24 22:40:32 -0700

    Improve output, parse flags properly

--- main.go
+++ main.go
@@ -9,16 +9,24 @@ import (
 )
 
 func main() {
-	proxyPort := flag.String("proxy-port", "9090", "Port that capitanhook proxy binds to")
-	targetURL := flag.String("target-host", "google.com", "Target URL to proxy to")
-	targetPort := flag.String("target-port", "80", "Target port to proxy to")
+
+	var (
+		proxyPort  string
+		targetURL  string
+		targetPort string
+	)
+
+	flag.StringVar(&proxyPort, "proxy-port", "9090", "Port that capitanhook proxy binds to")
+	flag.StringVar(&targetURL, "target-url", "google.com", "Target URL to proxy to")
+	flag.StringVar(&targetPort, "target-port", "80", "Target port to proxy to")
 	flag.Parse()
 	targetHost := fmt.Sprintf("%s:%s", targetURL, targetPort)
-	listenString := fmt.Sprintf(":%f", proxyPort)
+	listenString := fmt.Sprintf(":%v", proxyPort)
+
 	proxy := httputil.NewSingleHostReverseProxy(&url.URL{
 		Scheme: "http",
 		Host:   targetHost,
 	})
-	fmt.Printf("The capitan is listening %v", listenString)
+	fmt.Printf("The capitan is listening on port: %s, proxying traffic to %s", proxyPort, targetHost)
 	http.ListenAndServe(listenString, proxy)
 }
